4d9f6d7df3d28043a0330b52a08f74a5c2052c08
Eric De Witte <edewitte@vmware.com>
2019-01-01 14:27:02 +0100

    fix pantilt-flask.py

--- examples/pantiltweb/pantilt-flask.py
+++ examples/pantiltweb/pantilt-flask.py
@@ -53,11 +53,29 @@ def going_Right():
         pantilthat.pan(direction)
     return
 
+def going_Up():
+    global angle
+    angle = angle - 1
+    if angle < -88 or angle > 88:
+        return
+    else:
+        pantilthat.tilt(angle)
+    return
+
+def going_Down():
+    global angle
+    angle = angle + 1
+    if angle < -88 or angle > 88:
+        return
+    else:
+        pantilthat.tilt(angle)
+    return
+
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/<state>")
-def update_robot(state=None):
+def update_pantilt(state=None):
     if state == 'left':
         going_Left()
     if state == 'right':
@@ -66,37 +84,14 @@ def update_robot(state=None):
         going_Up()
     if state == 'downward':
 	    going_Down()
+    if state == 'lighton':
+        lightOn()
+    if state == 'lightoff':
+	    lightOff()
     template_data = {
         'title' : state,
     }
     return render_template('main.html', **template_data)
-
-@app.route('/lighton')
-def lightonRoute():
-    lightOn()
-    return "Switch Lights On"
-
-@app.route('/lightoff')
-def lightoffRoute():
-    lightOff()
-    return "Switch Lights Off"
-
-@app.route('/api/<direction>/<int:angle>')
-def api(direction, angle):
-    if angle < 0 or angle > 180:
-        return "{'error':'out of range'}"
-
-    angle -= 90
-
-    if direction == 'pan':
-        pantilthat.pan(angle)
-        return "{{'pan':{}}}".format(angle)
-
-    elif direction == 'tilt':
-        pantilthat.tilt(angle)
-        return "{{'tilt':{}}}".format(angle)
-
-    return "{'error':'invalid direction'}"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=9595, debug=True)
