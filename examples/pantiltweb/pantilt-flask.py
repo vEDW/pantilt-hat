53fe4c5c67327223e17af304654d99a1feda696a
Eric De Witte <edewitte@vmware.com>
2019-01-01 14:02:10 +0100

    fix pantilt-flask.py

--- examples/pantiltweb/pantilt-flask.py
+++ examples/pantiltweb/pantilt-flask.py
@@ -4,45 +4,43 @@
 # Import required Python libraries
 # -----------------------
 
-import explorerhat
+import pantilthat
 import time
+
+try:
+    from flask import Flask, render_template, request
+except ImportError:
+    exit("This script requires the flask module\nInstall with: sudo pip install flask")
+
 from flask import Flask, render_template, request
 
-timelength =  3
 lastmove = ""
 
+pantilthat.light_mode(pantilthat.WS2812)
+pantilthat.light_type(pantilthat.GRBW)
 
-def shutdown_server():
-    func = request.environ.get('werkzeug.server.shutdown')
-    if func is None:
-        raise RuntimeError('Not running with the Werkzeug Server')
-    func()
+def neoPixelLight(red, green, blue):
+    pantilthat.set_all(red, green, blue)
+    pantilthat.show()
+    return
 
+def lightOn():
+    neoPixelLight(255,255,255) 
+    return
 
-def going_Left():
-    global timelength
-    global lastmove
-    if lastmove == 'right':
-        explorerhat.motor.one.forward()
-        time.sleep(timelength*2)
-        explorerhat.motor.one.stop()
+def lightOff():
+    neoPixelLight(0,0,0) 
+    return
 
-    explorerhat.motor.one.forward()
-    time.sleep(timelength)
-    explorerhat.motor.one.stop()
-    lastmove = 'left'
+@app.route('/lighton')
+def lightonRoute():
+    lightOn()
+    return "Switch Lights On"
 
-def going_Right():
-    global timelength
-    global lastmove
-    if lastmove == 'left':
-        explorerhat.motor.one.backward()
-        time.sleep(timelength*2)
-        explorerhat.motor.one.stop()
-    explorerhat.motor.one.backward()
-    time.sleep(timelength)
-    explorerhat.motor.one.stop()
-    lastmove = 'right'
+@app.route('/lightoff')
+def lightoffRoute():
+    lightOff()
+    return "Switch Lights Off"
 
 app = Flask(__name__)
 
@@ -52,20 +50,33 @@ def update_robot(state=None):
     if state == 'left':
         going_Left()
     if state == 'right':
-	going_Right()
-    if state == 'kill':
-        shutdown_server()
+	    going_Right()
     template_data = {
         'title' : state,
     }
     return render_template('main.html', **template_data)
 
 
-@app.errorhandler(404)
-def not_found(error):
-	return "You're doomed !", 404
+@app.route('/')
+def home():
+    return render_template('gui.html')
 
+@app.route('/api/<direction>/<int:angle>')
+def api(direction, angle):
+    if angle < 0 or angle > 180:
+        return "{'error':'out of range'}"
 
+    angle -= 90
+
+    if direction == 'pan':
+        pantilthat.pan(angle)
+        return "{{'pan':{}}}".format(angle)
+
+    elif direction == 'tilt':
+        pantilthat.tilt(angle)
+        return "{{'tilt':{}}}".format(angle)
+
+    return "{'error':'invalid direction'}"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=9595, debug=True)
