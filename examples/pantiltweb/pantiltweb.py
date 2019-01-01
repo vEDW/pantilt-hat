c706d2ee0ac56c3eafd88c5cb3453d749097d3b0
Eric De Witte <edewitte@vmware.com>
2019-01-01 12:55:33 +0100

    update flask to switch ligths

--- examples/pantiltweb/pantiltweb.py
+++ examples/pantiltweb/pantiltweb.py
@@ -10,6 +10,32 @@ except ImportError:
 
 app = Flask(__name__)
 
+pantilthat.light_mode(pantilthat.WS2812)
+pantilthat.light_type(pantilthat.GRBW)
+
+def neoPixelLight(red, green, blue):
+    pantilthat.set_all(red, green, blue)
+    pantilthat.show()
+    return
+
+def lightOn():
+    neoPixelLight(255,255,255) 
+    return
+
+def lightOff():
+    neoPixelLight(0,0,0) 
+    return
+
+@app.route('/lighton')
+def lightonRoute():
+    lightOn()
+    return "Switch Lights On"
+
+@app.route('/lightoff')
+def lightoffRoute():
+    lightOff()
+    return "Switch Lights Off"
+
 @app.route('/')
 def home():
     return render_template('gui.html')
